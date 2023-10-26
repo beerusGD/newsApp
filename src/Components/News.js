23dbc9834d21ac76700d4b25e853f01ec1b54cd4
Shreyash Patil <shreyaspatil1710@gmail.com>
2023-10-26 11:28:52 +0530

    Solved Errors

--- src/Components/News.js
+++ src/Components/News.js
@@ -49,60 +49,7 @@ export class News extends Component {
     });
   }
 
-  // Fetching data from API
-  // async componentDidMount() {
-  // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey="Put Your API Key"&page=1&pageSize=${this.props.pageSize}`;
-  // this.setState({loading: true});
-  // // waiting for data
-  // let data = await fetch(url);
-  // // waiting for data get converted into json
-  // let parsedData = await data.json()
-  // console.log(parsedData);
-  // this.setState({articles: parsedData.articles,
-  //   totalResults: parsedData.totalResults,
-  //   loading: false
-  // })
-  // this.updateNews();
-  // }
-
-  // Handling Previous Button
-  // handlePreviousClick = async () => {
-  //   console.log("Previous");
-  // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey="Put Your API Key"&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-  // this.setState({loading: true});
-  // let data = await fetch(url);
-  // let parsedData = await data.json()
-  // console.log(parsedData);
-
-  // this.setState({
-  //     page: this.state.page - 1,
-  //     articles: parsedData.articles,
-  //     loading: false
-  //   })
-  // this.setState({ page: this.state.page - 1 });
-  // this.updateNews();
-  // };
-
-  // Handling Next Button
-  // handleNextClick = async () => {
-  // console.log("Next");
-  // if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))) {
-
-  //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey="Put Your API Key"&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-  //   this.setState({loading: true});
-  //   let data = await fetch(url);
-  //   let parsedData = await data.json()
-
-  //   this.setState({
-  //      page: this.state.page + 1,
-  //     articles: parsedData.articles,
-  //     loading: false
-  //   })
-  // }
-  // this.setState({ page: this.state.page + 1 });
-  // this.updateNews();
-  // };
-
+  
   // Fetching data from API
   async componentDidMount() {
     this.updateNews();
